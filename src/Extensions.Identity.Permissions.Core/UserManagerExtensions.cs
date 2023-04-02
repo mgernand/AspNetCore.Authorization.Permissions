a47a33fd575a412581488f9efd74e2d20f9702b7
Matthias Gernand <matthias.gernand@fluxera.com>
2023-04-02 11:54:26 +0200

    Aded extension methods to allow setting the tenant ID of a user.

--- src/Extensions.Identity.Permissions.Core/UserManagerExtensions.cs
+++ src/Extensions.Identity.Permissions.Core/UserManagerExtensions.cs
@@ -18,7 +18,7 @@
         /// <summary>
         ///     Gets the tenant ID for the specified <paramref name="user" />.
         /// </summary>
-        /// <param name="manager"></param>
+        /// <param name="manager">The user manager.</param>
         /// <param name="user">The user whose tenant name should be retrieved.</param>
         /// <returns>
         ///     The <see cref="Task" /> that represents the asynchronous operation, containing the name for the specified
@@ -40,17 +40,41 @@
 			return tenantId;
 		}
 
-		/// <summary>
-		///     Returns the Tenant ID claim value if present otherwise returns null.
-		/// </summary>
-		/// <param name="manager"></param>
-		/// <param name="principal">The <see cref="ClaimsPrincipal" /> instance.</param>
-		/// <returns>The Tenant ID claim value, or null if the claim is not present.</returns>
-		/// <remarks>
-		///     The Tenant ID claim is identified by
-		///     <see cref="PermissionClaimTypes.TenantIdClaimType" />.
-		/// </remarks>
-		public static string GetTenantId<TUser>(this UserManager<TUser> manager, ClaimsPrincipal principal) 
+        ///  <summary>
+        ///      Sets the given <paramref name="tenantId" /> for the specified <paramref name="user" />.
+        ///  </summary>
+        ///  <param name="manager">The user manager.</param>
+        ///  <param name="user">The user whose tenant id should be set.</param>
+        ///  <param name="tenantId">The tenant id to set.</param>
+        ///  <returns>
+        /// 		The <see cref="Task" /> that represents the asynchronous operation.
+        ///  </returns>
+        public static async Task SetTenantIdAsync<TUser>(this UserManager<TUser> manager, TUser user, string tenantId)
+			where TUser : class
+		{
+			manager.ThrowIfDisposed();
+			ArgumentNullException.ThrowIfNull(user);
+
+			PropertyInfo tokenPropertyInfo = manager.GetType().GetProperty("CancellationToken", BindingFlags.NonPublic | BindingFlags.Instance);
+			CancellationToken cancellationToken = (CancellationToken)(tokenPropertyInfo?.GetValue(manager) ?? CancellationToken.None);
+
+			PropertyInfo storePropertyInfo = manager.GetType().GetProperty("Store", BindingFlags.NonPublic | BindingFlags.Instance);
+			IUserStore<TUser> store = storePropertyInfo?.GetValue(manager) as IUserStore<TUser>;
+
+			await store.SetTenantIdAsync(user, tenantId, cancellationToken);
+		}
+
+        /// <summary>
+        ///     Returns the Tenant ID claim value if present otherwise returns null.
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <param name="principal">The <see cref="ClaimsPrincipal" /> instance.</param>
+        /// <returns>The Tenant ID claim value, or null if the claim is not present.</returns>
+        /// <remarks>
+        ///     The Tenant ID claim is identified by
+        ///     <see cref="PermissionClaimTypes.TenantIdClaimType" />.
+        /// </remarks>
+        public static string GetTenantId<TUser>(this UserManager<TUser> manager, ClaimsPrincipal principal) 
 			where TUser : class
 		{
 			manager.ThrowIfDisposed();
